6f00ddb72df1fcb6cc42f24ee00532f304d94dcb
Emmanuels-code <arocket04@gmail.com>
2025-01-08 20:58:15 +0100

    feature: added required elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import SettingsButton from './components/SettingsButton'
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
+    <main className="container mx-auto p-10">
       <h1 className="text-3xl font-bold mb-6">Object Detection Platform</h1>
       <div className="flex justify-between items-center mb-4">
         <ImageUpload />
